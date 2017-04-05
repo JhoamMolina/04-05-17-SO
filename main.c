2b66b5b685bf5aed31913bff657caf7e7e6fa950
MogurMolinomio <jhoam.sebastian.molina@correounivalle.edu.co>
2017-04-05 16:05:40 -0500

    Second commit

---
+++ main.c
@@ -0,0 +1,8 @@
+#include "fibo.h"
+#include <stdio.h>
+
+int main(int argc, int** argv){
+
+	printf("%d\n",fibo(7));
+	return 0;
+}
---
+++ vector.c
@@ -0,0 +1,7 @@
+int suma(int * a, int b){
+
+*a = *a + b;
+
+return *a;
+
+}
